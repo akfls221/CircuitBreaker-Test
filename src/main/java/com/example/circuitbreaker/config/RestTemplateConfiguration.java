777b1b10d527a2aa8e6048c9c0e4c428b89ff2d4
엄태권 <akfls221@gmail.com>
2023-07-12 03:16:26 +0900

    fix : 설정파일 위치 변경

--- src/main/java/com/example/circuitbreaker/config/RestTemplateConfiguration.java
+++ src/main/java/com/example/circuitbreaker/config/RestTemplateConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.circuitbreaker.api;
+package com.example.circuitbreaker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
